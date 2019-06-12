19f0e83ddd0f8372ee7f9f26a7a89b010ebda190
dirt0412 <robertdrwecki@gmail.pl>
2019-06-13 01:01:02 +0200

    new features

---
+++ src/common/error.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.errorHandler = (err, req, res, next) => {
+  console.log(res.headersSent);
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+};
--- src/index.js
+++ src/index.js
@@ -5,20 +5,36 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postsService = require("./service/postsService");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const { check, validationResult } = require("express-validator/check");
+const errorHandler = require("./common/error");
 
-app.post("/posts", (req, res) => {
-  postsService
-    .addPost(Post.fromRequestBody(req.body))
-    .then(post => {
-      res.status(201).json({
-        message: "Post created",
-        payload: post.toJSON()
+app.use(bodyParser.json());
+app.use(expressValidator());
+
+app.post(
+  "/posts",
+  [check("title").isLength({ min: 2 }), check("content").isLength({ min: 3 })],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    postsService
+      .addPost(Post.fromRequestBody(req.body))
+      .then(post => {
+        res.status(201).json({
+          message: "Post created",
+          payload: post.toJSON()
+        });
+      })
+      .catch(error => {
+        errorHandler.errorHandler(error, req, res, next);
       });
-    })
-    .catch(error => {
-      res.status(500).json({ error });
-    });
-});
+  }
+);
 
 app.get("/posts", (req, res) => {
   postsService
@@ -29,7 +45,7 @@ app.get("/posts", (req, res) => {
       });
     })
     .catch(error => {
-      res.status(500).json({ error });
+      errorHandler.errorHandler(error, req, res, next);
     });
 });
 
@@ -42,7 +58,7 @@ app.get("/posts/:postId", (req, res) => {
       });
     })
     .catch(error => {
-      res.status(500).json({ error });
+      errorHandler.errorHandler(error, req, res, next);
     });
 });
 
@@ -55,8 +71,17 @@ app.delete("/posts/:postId", (req, res) => {
       });
     })
     .catch(error => {
-      res.status(500).json({ error });
+      errorHandler.errorHandler(error, req, res, next);
     });
+});
+
+app.use(function(req, res) {
+  res.status(404);
+  res.json({
+    error: {
+      code: "Page not found - error 404"
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
